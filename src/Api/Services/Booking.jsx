6da3771ad8017ae7a2a72f6a3f6c6ff6f125287c
adnanfaikar <adnan.faika@gmail.com>
2024-03-23 04:43:55 +0700

    deleting console.log at Booking.jsx

--- src/Api/Services/Booking.jsx
+++ src/Api/Services/Booking.jsx
@@ -2,15 +2,7 @@ import { axiosInstance } from "../CoreApi";
 
 const handleBooking = async (serviceId, day, time, paymentMethod) => {
   const token = window.localStorage.getItem("token");
-  console.log("Token:", token);
 
-  console.log(serviceId, day, time, paymentMethod);
-  console.log({
-    service_id: serviceId,
-    day: day,
-    Time: time,
-    payment_method: paymentMethod,
-  });
   try {
     const response = await axiosInstance.post(
       "api/v1/service/treatment/booking",
